422832413e321e9a838cfb7fc66692d618eccee0
nate-covs <naco2555@colorado.edu>
2025-11-29 15:47:05 -0700

    Added employee builder pattern as well as a regular employee creation. Added tests as well.

--- app/src/main/java/ems/Employee.java
+++ app/src/main/java/ems/Employee.java
@@ -1,11 +1,113 @@
+package ems;
 
-//public class Employee {
-//    private final String name;
-//    private final String address;
-//    private final double age;
-//    private final double salary;
-//    private final double phoneNumber;
-//    private final String department;
-//    private final String position;
-//
-//}
+import java.sql.SQLException;
+
+public class Employee {
+    private final String name;
+    private final double employeeID;
+    private final String address;
+    private final double age;
+    private final double salary;
+    private final String phoneNumber;
+    private final String department;
+    private final String position;
+
+    public Employee(String name, double employeeID, String address, double age, double salary, String phoneNumber, String department, String position) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.address = address;
+        this.age = age;
+        this.salary = salary;
+        this.phoneNumber = phoneNumber;
+        this.department = department;
+        this.position = position;
+    };
+
+    public String getName() {
+        return name;
+    }
+    public double getEmployeeID() {
+        return employeeID;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public double getAge() {
+        return age;
+    }
+
+    private Employee(EmployeeBuilder employeeBuilder) {
+        this.name = employeeBuilder.name;
+        this.employeeID = employeeBuilder.employeeID;
+        this.address = employeeBuilder.address;
+        this.age = employeeBuilder.age;
+        this.salary = employeeBuilder.salary;
+        this.phoneNumber = employeeBuilder.phoneNumber;
+        this.department = employeeBuilder.department;
+        this.position = employeeBuilder.position;
+    }
+
+    public static class EmployeeBuilder {
+        private final String name;
+        private final double employeeID;
+
+        private String address = "";
+        private double age = 0;
+        private double salary = 0;
+        private String phoneNumber = "";
+        private String department = "";
+        private String position = "";
+
+        public EmployeeBuilder(String name, double employeeID) {
+            this.name = name;
+            this.employeeID = employeeID;
+        }
+
+        public EmployeeBuilder employeeAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public EmployeeBuilder employeeAge(double age) {
+            this.age = age;
+            return this;
+        }
+
+        public EmployeeBuilder employeeSalary(double salary) {
+            this.salary = salary;
+            return this;
+        }
+
+        public EmployeeBuilder employeePhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public EmployeeBuilder employeeDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public EmployeeBuilder employeePosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public Employee buildEmployee() {
+            return new Employee(this);
+        }
+    }
+
+}
---
+++ app/src/test/java/ems/EmployeeTest.java
@@ -0,0 +1,33 @@
+package ems;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmployeeTest {
+
+    @Test
+    void testEmployeeCreation() {
+        Employee joe = new Employee("Joe", 1001, "123 Main St", 34,
+                75000, "303-303-3030", "Engineering", "Developer");
+
+        assertTrue(Objects.equals(joe.getName(), "Joe"));
+        assertTrue(joe.getEmployeeID() == 1001);
+    }
+    @Test
+    void testEmployeeBuilder(){
+        Employee bob = new Employee.EmployeeBuilder("Joe", 1001)
+                .employeeDepartment("Engineering")
+                .employeeSalary(75000)
+                .buildEmployee();
+
+        assertTrue(Objects.equals(bob.getName(), "Joe"));
+        assertTrue(bob.getEmployeeID() == 1001);
+        assertTrue(bob.getSalary() == 75000);
+        assertTrue(Objects.equals(bob.getDepartment(), "Engineering"));
+        assertTrue(bob.getAge() == 0);
+        assertTrue(Objects.equals(bob.getPosition(), ""));
+    }
+}
